473adab7a222b15e2cababc9974fb58a0d47f2d2
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2024-03-02 13:34:06 -0500

    Matrixx-ify

--- lineage_lynx.mk
+++ lineage_lynx.mk
@@ -8,6 +8,27 @@
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Matrixx
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_MAINTAINER := Pyrtle93
+MATRIXX_CHIPSET := TensorG2
+MATRIXX_BATTERY := 4385mah
+MATRIXX_DISPLAY := 1080x2400
+
+# Matrixx Flags
+WITH_GMS := true
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_PIXEL_FRAMEWORK := true
+TARGET_USES_GOOGLE_DIALER_CARRIER_SETTINGS :=true
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_EXCLUDES_AUXIO := true
+TARGET_EXCLUDES_VIA := true
+TARGET_PIXEL_BOOT_ANIMATION_RES := 1080
+TARGET_IS_PIXEL := true
+TARGET_IS_PIXEL_7 := true
+TARGET_PIXEL_STAND_SUPPORTED := true
+
 # Inherit device configuration
 $(call inherit-product, device/google/lynx/aosp_lynx.mk)
 $(call inherit-product, device/google/gs201/lineage_common.mk)
@@ -17,10 +38,6 @@ $(call inherit-product, device/google/lynx/device-lineage.mk)
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 7a
 PRODUCT_NAME := lineage_lynx
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=lynx \
